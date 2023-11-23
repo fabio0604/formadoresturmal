b525119c75557d844c53f655dd9298da7362315c
fabio0604 <129704222+fabio0604@users.noreply.github.com>
2023-11-23 09:12:49 -0300

    slides

---
+++ script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex - 1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Altere o tempo (em milissegundos) para mudar os slides automaticamente
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  showSlides();
+}
